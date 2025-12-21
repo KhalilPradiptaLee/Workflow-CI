7e54f9e55668bb03623cb44d05f5dd48b1bd3deb
KhalilPradiptaLee <144757000+KhalilPradiptaLee@users.noreply.github.com>
2025-12-22 01:04:33 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -16,61 +16,78 @@ experiment_name = "Student Performance - Modelling"
 def run_model():
 
     # =========================
-    # MLflow setup (local)
+    # MLflow setup
     # =========================
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
     mlflow.set_experiment(experiment_name)
 
-    # Autolog (WAJIB untuk Basic)
     mlflow.sklearn.autolog()
     print("Training dimulai...")
 
     # =========================
     # Load dataset
     # =========================
-    try:
-        df = pd.read_csv(path)
-        print("Dataset berhasil diload")
-    except FileNotFoundError:
-        print(f"Dataset tidak ditemukan di {path}")
-        return
+    df = pd.read_csv(path)
+    print("Dataset berhasil diload")
 
     # =========================
     # Feature & target
     # =========================
-    x = df.drop(columns=["FinalGrade"])
+    X = df.drop(columns=["FinalGrade"])
     y = df["FinalGrade"]
 
     # =========================
     # Train-test split
     # =========================
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.2, random_state=42
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
     )
 
     # =========================
     # Training
     # =========================
-    with mlflow.start_run(run_name="RandomForest-Baseline"):
+    with mlflow.start_run(run_name="RandomForest-Baseline") as run:
+        run_id = run.info.run_id
+        with open("run_id.txt", "w") as f:
+            f.write(run_id)
+
+        print(f"Run ID disimpan: {run_id}")
+
         model = RandomForestRegressor(
             n_estimators=100,
             random_state=42
         )
 
-        model.fit(x_train, y_train)
+        model.fit(X_train, y_train)
 
         # =========================
         # Evaluation
         # =========================
-        y_pred = model.predict(x_test)
+        y_pred = model.predict(X_test)
 
         mae = mean_absolute_error(y_test, y_pred)
         rmse = np.sqrt(mean_squared_error(y_test, y_pred))
         r2 = r2_score(y_test, y_pred)
 
+        mlflow.log_metrics({
+            "MAE": mae,
+            "RMSE": rmse,
+            "R2": r2
+        })
+
         print("MAE :", mae)
         print("RMSE:", rmse)
         print("R2  :", r2)
+
+        # =========================
+        # Log model
+        # =========================
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=X_test.head(5)
+        )
+
+        print("Model berhasil disimpan sebagai artifact MLflow")
 
 
 if __name__ == "__main__":
